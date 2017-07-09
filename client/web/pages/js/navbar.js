ef4711ecfb0abb43cfa50089750b076ec2434793
My Name <my-name@chromium.org>
2017-07-09 02:08:52 -0400

    Renamed Files and Javascript Events

--- client/web/pages/js/navbar.js
+++ client/web/pages/js/navbar.js
@@ -1,4 +1,24 @@
-function openMe()
-{
-document.write("Hello World!");
-}
+document.getElementById("NavMe").onmousedown = function () {
+    console.log("User moused down on ME");
+    return true; // Not needed, as long as you don't return false
+};
+document.getElementById("NavStuff").onmousedown = function () {
+    console.log("User moused down on STUFF");
+    return true; // Not needed, as long as you don't return false
+};
+document.getElementById("NavGames").onmousedown = function () {
+    console.log("User moused down on GAMES");
+    return true; // Not needed, as long as you don't return false
+};
+document.getElementById("NavRooms").onmousedown = function () {
+    console.log("User moused down on ROOMS");
+    return true; // Not needed, as long as you don't return false
+};
+document.getElementById("NavGroups").onmousedown = function () {
+    console.log("User moused down on GROUPS");
+    return true; // Not needed, as long as you don't return false
+};
+document.getElementById("NavShop").onmousedown = function () {
+    console.log("User moused down on SHOP");
+    return true; // Not needed, as long as you don't return false
+};
